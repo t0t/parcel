099311f7c528e5d5386bf179c86570eabfa90282
t0t <sergiofores@gmail.com>
2021-11-09 16:41:09 +0100

    public folder added for vercel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,10 @@
-import "../estilos/main.scss";
-import {getFullName} from "./utils";
-
 const moment = require("moment");
+const d3 = require("d3");
+
+
+import "../estilos/main.scss";
+import {getFullName} from "./utils/utils";
+
 
 const firstName = getFullName("JStestOK");
 console.log(firstName)
@@ -10,3 +13,6 @@ console.log(firstName)
 const now = moment().format("YYYY-MM-DD, h:mm:ss a")
 
 console.log(now)
+console.log(d3)
+
+
